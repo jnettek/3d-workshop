5213dd8086dc27243b6a4cfec749512edb495841
jnettek <jnettekpelletier@live.com>
2023-11-01 13:46:28 -0400

    camera setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-
 import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { AxesHelper } from 'three';
 import { Suspense } from 'react';
 import { Physics } from '@react-three/cannon';
-// import RotatingBox from './components/RotatingBox';
 import Background from './components/Background';
 import Floor from './components/Floor';
 import ColorPicker from './components/ColorPicker';
 import Bulb from './components/Bulb';
-import Dragable from './components/Dragable';
-import Model from './components/Model';
-import BoundingBox from './components/BoundingBox';
-
-
-
+import Cars from './components/Cars';
+import CameraContorls from './components/CameraControls';
 
 
 function App() {
@@ -28,36 +22,11 @@ function App() {
             <ColorPicker />
             <Canvas style={{background: 'black'}}
             camera={{ position: [7,7,7]}} shadowMap>
+                <CameraContorls orbitRef={orbitRef}/>
                 <ambientLight intensity={0.2} />
                 <Physics>
                 <Bulb/>
-                <Suspense fallback={null}>
-                <Dragable orbitControlsRef={orbitRef}>
-                <BoundingBox visible 
-                position={[-4,4,0]}
-                dims={[2,1.5,5]}>
-                    <Model 
-                    path='/tesla_2018_model_3/scene.gltf'
-                    scale={new Array(3).fill(0.01)}
-                    
-                    />
-                </BoundingBox>
-                </Dragable>
-                </Suspense>
-                <Suspense fallback={null}>
-                    <Dragable orbitControlsRef={orbitRef}p>
-                        <BoundingBox visible
-                        position={[4,4,0]}
-                        dims={[2,1,5]}
-                        offset={[-0.1,-0.5,-0.5]}
-                        >
-                            <Model 
-                            path='/tesla_model_y/scene.gltf'
-                            scale={new Array(3).fill(0.01)}
-                            />
-                </BoundingBox>
-                    </Dragable>
-                </Suspense>
+                <Cars orbitRef={orbitRef}/>
                 <Suspense fallback={null}>
                     <Background />
                 </Suspense>
---
+++ src/components/Cars.jsx
@@ -0,0 +1,42 @@
+import { Suspense } from "react";
+import BoundingBox from "./BoundingBox";
+import Dragable from "./Dragable";
+import Model from './Model';
+
+
+const Cars = ({ orbitRef }) => {
+    return (         
+        <group>
+
+    <Suspense fallback={null}>
+        <Dragable orbitControlsRef={orbitRef}>
+        <BoundingBox visible 
+        position={[-4,4,0]}
+        dims={[2,1.5,5]}>
+            <Model 
+            path='/tesla_2018_model_3/scene.gltf'
+            scale={new Array(3).fill(0.01)}
+            
+            />
+        </BoundingBox>
+        </Dragable>
+        </Suspense>
+        <Suspense fallback={null}>
+            <Dragable orbitControlsRef={orbitRef}p>
+                <BoundingBox visible
+                position={[4,4,0]}
+                dims={[2,1,5]}
+                offset={[-0.1,-0.5,-0.5]}
+                >
+                    <Model 
+                    path='/tesla_model_y/scene.gltf'
+                    scale={new Array(3).fill(0.01)}
+                    />
+        </BoundingBox>
+            </Dragable>
+        </Suspense>
+                    </group>
+        );
+}
+
+export default Cars;
